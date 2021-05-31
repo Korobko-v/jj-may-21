11f56fb8dd6ecc66960a088c3446e2142096a203
korobko <19kvd95@mail.ru>
2021-05-31 22:15:09 +0300

    testMul

--- src/test/java/ru/levelp/jj/LogicTest.java
+++ src/test/java/ru/levelp/jj/LogicTest.java
@@ -25,4 +25,11 @@ public class LogicTest {
         assertEquals(3, new Logic().sum(2,1));
         assertEquals(0, new Logic().sum(0,0));
     }
+
+    @Test
+    public  void testMul() {
+        assertEquals(6, new Logic().multiply(3,2));
+        assertEquals(12, new Logic().multiply(3,4));
+        assertEquals(100, new Logic().multiply(20,5));
+    }
 }
